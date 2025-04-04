9c64d050840b24034e1a137a2144d4e01b28d864
Darksword333 <mazetgabriel@gmail.com>
2025-04-04 15:31:16 +0200

    TP1 Exo4 marche presque (père ne s'arrête pas quand on lit x char, x>NB)

--- TP/TP1/exo4.c
+++ TP/TP1/exo4.c
@@ -11,11 +11,11 @@ int compteur_fils[NB_FILS] = {0, 0};
 
 void SigUSR_handler(int sig) {
     if (sig == SIGUSR1) {
-        printf(">> SIGUSR1 reçu par %d\n", getpid());
+        printf(">> SIGUSR1 envoyé par fils [%d]\n", getpid());
         compteur_fils[0]++;
     }
     else if (sig == SIGUSR2) {
-        printf(">> SIGUSR2 reçu par %d\n", getpid());
+        printf(">> SIGUSR2 envoyé par fils [%d]\n", getpid());
         compteur_fils[1]++;
     }
 }
@@ -28,15 +28,16 @@ int main(int argc, char *argv[]){
     int NB = atoi(argv[1]);
     int nb_lu = 0;
     int NBMAX = atoi(argv[2]);
-    char c;
-    pid_t ppid;
+    char buffer[NB];
+    pid_t ppid, pid;
 
-    sigset_t mask;
+    sigset_t mask, oldmask; // Pour changer de masque de signal
     sigfillset(&mask);
     sigdelset(&mask, SIGUSR1);
     sigdelset(&mask, SIGUSR2);
+    sigprocmask(SIG_BLOCK, &mask, &oldmask);
 
-    struct sigaction act; // Pour masquer puis demasquer
+    struct sigaction act;
     act.sa_handler = SigUSR_handler;
     act.sa_flags = 0;
     sigemptyset(&act.sa_mask);
@@ -45,33 +46,31 @@ int main(int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
     for (int i = 0; i < 2; i++) {
-        switch (fork()) {
+        switch (pid = fork()) {
             case -1:
                 perror("fork");
                 exit(EXIT_FAILURE);
             case 0: // Processus Fils
                 ppid = getppid();
-                while (nb_lu < NBMAX) {
-                    for (int j = 0; j < NB; j++) {
-                        if (scanf("%c", &c) == EOF) {
-                            exit(EXIT_SUCCESS);
-                        }
-                        
-                    }
+                pid = getpid();
+                while (nb_lu != NBMAX) {
+                    for (int j = 0; j < NB; j++)
+                        scanf(" %1c", &buffer[j]); // Lis 1 char et ne lis pas les "\n", "\t" " "
                     nb_lu+=NB;
-                    printf("%d", nb_lu);
-                    if (i == 0)
-                        kill(ppid, SIGUSR1);
-                    else 
-                        kill(ppid, SIGUSR2);
+                    printf("===Fils [%d] : J'ai lu %s, j'ai lu au total %d\n", pid, buffer, nb_lu);
+                    kill(ppid, (i == 0) ? SIGUSR1 : SIGUSR2); //If premier fils SIGUSR1, else SIGUSR2
                 }
-                printf(">> %d termine\n", getpid());
+                printf(">> Fils [%d] termine\n", pid);
                 exit(EXIT_SUCCESS);
             default: // Processus Père
-                if (sigsuspend(&mask) == -1) {
-                    perror("sigsuspend");
-                    exit(EXIT_FAILURE);
+                while (compteur_fils[i]*NB < NBMAX) { //Le handler rajoute que un par un
+                    if (sigsuspend(&mask) != -1) {
+                        perror("Erreur sigsuspend\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    printf("===Père : Mon fils [%d] a lu %d\n", pid, compteur_fils[i]*NB);
                 }
+                wait(NULL); // Attends ses fils
                 break;
         }
     }
